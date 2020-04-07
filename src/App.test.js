f20e8bca809c15b59bbce599f03e2a0c663401f6
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-04-06 20:51:33 -0700

    added files

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+
 it("renders App without crashing", () => {
   render(<App />);
+});a
+
+describe('UNIT TESTING', () => {
+  
+  it('renders without crashing', () => {
+    const wrapper = render(
+      <span className="greet">hello world</span>
+    );
+    // console.log(wrapper.debug())
+    const element = wrapper.queryByText(/hello world/i);
+    expect(element).toBeInTheDocument();
+    expect(element).toBeTruthy();
+    expect(element).toBeVisible();
+  });
+
 });
